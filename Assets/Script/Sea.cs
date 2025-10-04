1c2a34137f4d2f421f27f6df5984e02c70460738
starmatch9 <18534538017@163.com>
2025-10-04 18:14:53 +0800

    加入海平面

---
+++ Assets/Script/Sea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sea : MonoBehaviour
+{
+    //后续随雨的强度变化
+    [Header("海平面上升速度")]
+    public float speed = 5f;
+    
+    bool isRising = false;
+
+    public void StartRising()
+    {
+        isRising = true;
+    }
+    public void StopRising()
+    {
+        isRising = false;
+    }
+
+    private void Update()
+    {
+        if (isRising)
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+    }
+
+}
